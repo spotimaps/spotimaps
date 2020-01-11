d938de5e6fae0770c18e9df142ee3c30d92a6e1b
Ryan Yang <ryanqyang@gmail.com>
2020-01-11 11:49:27 -0800

    Updates

---
+++ precache-manifest.3cc403a23186eb42c3075432fc48d746.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddda3c2b2f337413371cda0726307728",
+    "url": "/spotimaps/index.html"
+  },
+  {
+    "revision": "19c62a6c4dbbe76eb720",
+    "url": "/spotimaps/static/css/2.36f280e5.chunk.css"
+  },
+  {
+    "revision": "733873598cbf1635b5fc",
+    "url": "/spotimaps/static/css/main.20393124.chunk.css"
+  },
+  {
+    "revision": "19c62a6c4dbbe76eb720",
+    "url": "/spotimaps/static/js/2.d064636a.chunk.js"
+  },
+  {
+    "revision": "5ce8e17bd75f7f932607c312cddf0daa",
+    "url": "/spotimaps/static/js/2.d064636a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "733873598cbf1635b5fc",
+    "url": "/spotimaps/static/js/main.1fa65074.chunk.js"
+  },
+  {
+    "revision": "00cd15b45421346eca8f",
+    "url": "/spotimaps/static/js/runtime-main.e722f594.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8a6e77868ed96d1e2f0edaccf642403.js"
+  "/spotimaps/precache-manifest.3cc403a23186eb42c3075432fc48d746.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotimaps/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
