5b425fdeefe05fb9eb1bcf6f6d30f0f882d7a4e1
jcwang111 <joshuacwang@gmail.com>
2020-01-11 15:31:43 -0800

    edited the startmodal

--- src/components/StartModal.js
+++ src/components/StartModal.js
@@ -1,5 +1,9 @@
 export default `
 <div class="start-modal--wrapper">
-    Gello wrlkds
+    <h1>Welcome to Spotimaps! :D</h1>
+    <img src="/img1.png" alt="Icon" style="width:384px;height:384px;"> <br/>
+    Click around to see what people are listening to! <br/> 
+    This application uses the Spotify API.<br/> 
+    
 </div>
 `;
