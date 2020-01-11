997d957cabd563adbf40dc47c547020c6c572461
nicholaswang610 <wangnich@gmail.com>
2020-01-11 14:30:31 -0800

    popup design

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -4,7 +4,13 @@ import './Marker.css';
 const Marker = ({info}) => {
     return (
         <div className="marker-wrapper">
-            {JSON.stringify(info)}
+            <div id="song">
+                <b>{info.name}</b>
+                <img id="speaker" src={require("./speaker_icon.png")} alt="speaker" width="12" height="12"/>
+            </div>
+            <div id="artist">
+                <b>{info.artist}</b>
+            </div>
         </div>
     )
 }
